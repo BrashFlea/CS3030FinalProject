ed9c3ce0c869902ba8eb71caac3a6f0f7c32ab48
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2016-12-06 12:00:22 -0700

    Report generating logic working and validated. Need to move logic to function and create help function

--- jonathan_mirabile_final.rb
+++ jonathan_mirabile_final.rb
@@ -9,7 +9,8 @@
 # Recreation of Python Homework 6 using Ruby
 # irb opens the ruby REPL
 require 'open-uri'
-uri = open('http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.test')
+
+uri = open('http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.full')
 contents = uri.read
 data = Array[]
 i = 0
@@ -17,12 +18,13 @@ contents.scan(/(?:\[.*?\]) (?:\[error\]) (?:\[.*?\]) (.*)/) do |match|
 	data[i] = match
 	i += 1
 end
+#Cast to a hash to count errors
 counts = Hash.new 0
 data.each do |match|
 	counts[match] += 1
 end
-
-counts.keys[1..25].each { |key| puts "#{counts[key]} => #{key}" }
+puts "*** Top 25 Errors***"
+counts.keys.sort_by{|key| counts[key]}.reverse.each_with_index { |key, index| puts "Count: #{counts[key]} Error: #{key}" if index < 25 }
 
 
 
