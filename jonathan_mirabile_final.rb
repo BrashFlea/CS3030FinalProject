bff5496d800aebdf5be4e7d5bfe756a2c826897e
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2016-12-06 23:39:16 -0700

    Added help function and command line argument checks

--- jonathan_mirabile_final.rb
+++ jonathan_mirabile_final.rb
@@ -6,27 +6,50 @@
 # Distributed under terms of the MIT license.
 #
 # Final project for CS3030
-# Recreation of Python Homework 6 using Ruby
-# irb opens the ruby REPL
+# Recreation of Python Homework 6 - Apache error log report tool using Ruby
+# Note: report best viewed in full screen as errors are verbose
 require 'open-uri'
+arg = ARGV[0]
 
-uri = open('http://icarus.cs.weber.edu/~hvalle/cs3030/data/error.log.full')
-contents = uri.read
-data = Array[]
-i = 0
-contents.scan(/(?:\[.*?\]) (?:\[error\]) (?:\[.*?\]) (.*)/) do |match|
-	data[i] = match
-	i += 1
+
+def help()
+	puts "Usage is ./jonathan_mirabile_final.rb <file input>"
 end
-#Cast to a hash to count errors
-counts = Hash.new 0
-data.each do |match|
-	counts[match] += 1
+
+
+def fetch_url(arg)
+	contents = open(arg).read
+	data = Array[]
+	i = 0
+	#Regex checks for 4 groups:
+	#1: []
+	#2: [error]
+	#3: []
+	#4: Rest of string
+	#Then ignores groups 1-3 and leaves group 4 (the verbose error) 
+	contents.scan(/(?:\[.*?\]) (?:\[error\]) (?:\[.*?\]) (.*)/) do |match|
+		data[i] = match
+		i += 1
+	end
+	#Create a hash to count errors
+	counts = Hash.new 0
+	data.each do |match|
+		counts[match] += 1
+	end
+	puts "*** Top 25 Errors***"
+	counts.keys.sort_by{|key| counts[key]}.reverse.each_with_index { |key, index| puts "Count: #{counts[key]} Error: #{key}" if index < 25 }
 end
-puts "*** Top 25 Errors***"
-counts.keys.sort_by{|key| counts[key]}.reverse.each_with_index { |key, index| puts "Count: #{counts[key]} Error: #{key}" if index < 25 }
 
 
+unless ARGV.length == 1
+	help()
+	exit(1)
+end
+
+
+puts "Fetching file from #{ARGV[0]}"
+fetch_url(arg)
+exit(0)
 
 
 
